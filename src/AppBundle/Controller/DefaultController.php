08eb748d6fb397364bc06b8b4a79ae1088590ed1
Ahmed Mustapha <ahmedmust@hotmail.fr>
2018-07-24 11:55:01 +0200

    refactor: organiser le code
    Ajouter le titre de l'autheur dans le code
    
    fixes issue #122

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,7 +4,9 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
+/**
+ * Author Ahmed MUSTAPHA
+ */
 class DefaultController extends Controller
 {
     /**
